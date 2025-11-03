db38e19e1dbd5a2340c3737f803c258bd6f6a3ba
Akash Kushwaha <akashkushwaha885612@gmail.com>
2025-11-03 20:49:43 +0530

    String indexing example in.cpp
    Demonstrate string indexing by modifying the first character.

---
+++ Sting indexing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main (){
+    string s = "Gaurav kushwaha";
+        cout<<s<<endl;
+        s[0] = 'S';
+        cout<<s<<endl;
+    return 0;
+}
