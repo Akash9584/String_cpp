1a09290fa2af04249fe55323e5b97dd7f5e1923c
Akash Kushwaha <akashkushwaha885612@gmail.com>
2025-11-03 20:50:56 +0530

    Check Length program to display string length

---
+++ Check Length.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    string a = "Gaurav kushwaha";
+        cout<<a.length()<<endl;
+        cout<<'\0'; // \0 = null - ASCII Value = 0;
+    return 0;
+}
